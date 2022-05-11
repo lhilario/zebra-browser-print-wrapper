1dd8e335f96ef264fea692a297a86dd1777b1f5d
Patrick van de Glind <patrick.glind@hitachivantara.com>
2022-05-11 16:23:00 +0200

    Push lib folder to Github for usability

---
+++ lib/constants.d.ts
@@ -0,0 +1,1 @@
+export declare const API_URL = "http://localhost:9100/";
---
+++ lib/types.d.ts
@@ -0,0 +1,9 @@
+export interface Device {
+    name: string;
+    deviceType: string;
+    connection: string;
+    uid: string;
+    provider: string;
+    manufacturer: string;
+    version: number;
+}
